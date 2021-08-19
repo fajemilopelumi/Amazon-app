77178e146296b0938d44197e6ef455be3bfca9c9
unknown <pfajemilo@gmail.com>
2021-08-19 15:22:08 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,114 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: "Home Fridge",
+            category: "Electronic",
+            image:'/images/p1.jpeg',
+            price: 120,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '2',
+            name: "Home Fridge",
+            category: "Electronic",
+            image:'/images/p2.jpeg',
+            price: 190,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '3',
+            name: "Plasma",
+            category: "Electronic",
+            image:'/images/p3.jpeg',
+            price: 120,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '4',
+            name: "Buggati",
+            category: "Car",
+            image:'/images/p4.jpeg',
+            price: 1200,
+            brand: 'LG',
+            rating: 3,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '5',
+            name: "Nimosene",
+            category: "Car",
+            image:'/images/p5.jpeg',
+            price: 1000,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '6',
+            name: "Benz",
+            category: "Car",
+            image:'/images/p6.jpeg',
+            price: 1000,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '7',
+            name: "NBMW",
+            category: "Car",
+            image:'/images/p7.jpeg',
+            price: 1000,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '8',
+            name: "RSV",
+            category: "Motocycle",
+            image:'/images/p8.jpeg',
+            price: 1000,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+        {
+            _id: '9',
+            name: "Power Bike",
+            category: "MOtocycle",
+            image:'/images/p9.jpeg',
+            price: 1000,
+            brand: 'LG',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+
+        },
+    ],
+}
+
+export default data
